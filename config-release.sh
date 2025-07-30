e649d059617a12a130fa2c94fe610d615d22e7d1
wurikiji <wurikiji@gmail.com>
2025-07-30 23:53:38 +0900

    chore: add config files

---
+++ config-release.sh
@@ -0,0 +1,7 @@
+cmake -DCMAKE_BUILD_TYPE=Release -DWITH_DEBUG=0 -DWITH_UNIT_TESTS=0 \
+	-DCMAKE_C_COMPILER=clang \
+	-DCMAKE_CXX_COMPILER=clang++ \
+	-DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=lld -L/usr/local/lib/x86_64-linux-gnu" \
+	-DCMAKE_MODULE_LINKER_FLAGS="-fuse-ld=lld -L/usr/local/lib/x86_64-linux-gnu" \
+	-DCMAKE_SHARED_LINKER_FLAGS="-fuse-ld=lld -L/usr/local/lib/x86_64-linux-gnu" \
+	-B build
